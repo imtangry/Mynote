2567d5e88a1119b3fbb07e54f50c6320226341a8
汤瑞宇 <tang.rui.yu@foxmail.com>
2018-07-07 11:45:40 +0800

    ADD

---
+++ 作业/project/src/view/about/index.js
@@ -0,0 +1,7 @@
+import {Component} from "react";
+class About extends Component{
+    render(){
+        return "关于"
+    }
+}
+export default About;
---
+++ 作业/project/src/view/detail/index.js
@@ -0,0 +1,7 @@
+import {Component} from "react";
+class Detail extends Component{
+    render(){
+        return "更多"
+    }
+}
+export default Detail;
