ff537ef664866353295e77d82f9627e3b844df32
汤瑞宇 <tang.rui.yu@foxmail.com>
2018-07-16 23:57:56 +0800

    ADD

--- 作业/hm0714/js/vue.js
+++ 作业/hm0714/js/vue.js
@@ -9,7 +9,24 @@ var index_hot = [
         {title: "眼部修复：肌透眼部精华霜、小棕瓶护眼精华", img: "./img/s2.jpg", link: "#"}],
     index_tutorial = [{title: "", video_src: "img/ex.mp4", img_src: "img/v.jpg"},
         {title: "", video_src: "img/ex.mp4", img_src: "img/v.jpg"},
-        {title: "", video_src: "img/ex.mp4", img_src: "img/v.jpg"}];
+        {title: "", video_src: "img/ex.mp4", img_src: "img/v.jpg"}],
+    goods_type = {
+        face: [{belongs: "face", type: "卸妆乳", link: ""},
+            {belongs: "face", type: "洁面乳", link: ""},
+            {belongs: "face", type: "BB霜", link: ""},
+            {belongs: "face", type: "粉底液", link: ""},
+            {belongs: "face", type: "腮红", link: ""}],
+        eye: [{belongs: "face", type: "眼影", link: ""},
+            {belongs: "face", type: "眼线笔", link: ""},
+            {belongs: "face", type: "卧蚕笔", link: ""},
+            {belongs: "face", type: "睫毛膏", link: ""},
+            {belongs: "face", type: "眉粉", link: ""}],
+        lips: [
+            {belongs: "face", type: "唇彩", link: ""},
+            {belongs: "face", type: "唇部打底", link: ""},
+            {belongs: "face", type: "遮瑕膏", link: ""}
+        ]
+    }
 // 全局注册
 Vue.component('index_content_header', {
     props: ['start', 'other', 'flag'],
@@ -26,11 +43,27 @@ Vue.component('h5video', {
     "                    </video>\n" +
     "                </div>"
 });
+Vue.component("step_line", {
+    props: ["step", "types"],
+    template: "<div class=\"step step_now\">\n" +
+    "                <div class=\"step_num item\">{{step}}</div>\n" +
+    "<div class=\"item\" v-for=\"item in types\">\n" +
+    "                            {{item.type}}\n" +
+    "                        </div>" +
+    "            </div>"
+})
 var app_index = new Vue({
     el: "#index_content",
     data: {
         index_hot: index_hot,
         index_sales: index_sales,
-        index_tutorial: index_tutorial
+        index_tutorial: index_tutorial,
+        goods_type:goods_type
     }
 });
+var app_hedaer = new Vue({
+    el: "#header",
+    data: {
+        goods_type: goods_type
+    }
+})
