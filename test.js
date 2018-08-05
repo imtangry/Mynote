4e679281baea1ffcec99f8ab56c9b514f21dcf11
汤瑞宇 <tang.rui.yu@foxmail.com>
2018-08-05 15:00:22 +0800

    ADD

--- test.js
+++ test.js
@@ -20,9 +20,46 @@
 // console.log(p1.friends);
 // console.log(p1.name);
 
-var str =`aaa`;
-var b =Object.create({a:1});
-console.log(typeof str.__proto__);
-console.log(str.__proto__);
-// console.log(b.prototype.a);
-console.log(Object.getPrototypeOf(str));
+// var str =`aaa`;
+// var b =Object.create({a:1});
+// console.log(typeof str.__proto__);
+// console.log(str.__proto__);
+// // console.log(b.prototype.a);
+// console.log(Object.getPrototypeOf(str));
+// function a() {
+//     this.p = 1
+// }
+// a.prototype.getp=function () {
+//     return this.p
+// }
+// function b() {
+//     this.q = 2
+// }
+// b.prototype = new a();
+// b.prototype.getq=function () {
+//     return this.q;
+// }
+// var i = new b()
+// console.log(i.getp());
+
+
+async function test() {
+    await new Promise((resolve => {
+        setTimeout(function () {
+            console.log(1);
+        }, 1000)
+    }));
+    await new Promise((resolve => {
+        setTimeout(function () {
+            console.log(2);
+        }, 1000)
+    }));
+    await new Promise((resolve => {
+        setTimeout(function () {
+            console.log(3);
+        }, 1000)
+    }));
+    console.log(4);
+}
+
+test().then().then().then()
